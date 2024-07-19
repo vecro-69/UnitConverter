3b610b8e9c5c8f46feb78b6d5f06b85ef1f39f5f
Vecro <victor.taulean@outlook.com>
2024-07-19 14:35:59 +0300

    Added Spacer

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.unitconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -16,8 +19,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,13 +44,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
         // Here all the UI elements will be stacked below each other
         Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {
             //here goes what should happen, when the Value of our OutlinedTextField changes.
         })
-
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
             // Here all the UI elements will be stacked next to each other
             Box {
@@ -67,6 +78,7 @@ fun UnitConverter() {
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Result: ")
     }
 }
